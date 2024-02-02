100a5288f3151f5b003aa2e48cd1c13b6770a1ff
JaviTank <158140993+JaviTank13@users.noreply.github.com>
2024-02-02 12:47:32 +0100

    Actualizado Usuario.java añadido Constructor
    He añadido el constructor para el posterior metodo y utilizacion del codigo en Principal.java

--- Usuario.java
+++ Usuario.java
@@ -1,6 +1,14 @@
 public class Usuario {
-    String nombre;
+    
+	String nombre;
     String apellidos;
     String email;
+    
+    public Usuario(String nombre, String apellidos, String email) {
+		super();
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.email = email;
+	}
     
 }
