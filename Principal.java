7ef43b065663df05f2da275913cd30fe62c07948
JaviTank <158140993+JaviTank13@users.noreply.github.com>
2024-02-02 12:55:58 +0100

    Actualizado y terminado Principal.java
    Añadidas las especificaciones que mencionas en el ejercicio, cabe destacar que he tenido que buscar ayuda con el Overflow porque un metodo no me ha salido demasiado bien.

--- Principal.java
+++ Principal.java
@@ -1,13 +1,45 @@
-import java.util.Date;
+import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
-        fechaDeHoy();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese un número para crear el array de Usuarios: ");
+        int tamanyo = sc.nextInt();
+
+        Usuario[] usuarios = arrayUsur(tamanyo);
+
+        System.out.println("---------------------|Informacion Usuario|---------------");
+        for (Usuario usuario : usuarios) {
+            System.out.println("Nombre: " + usuario.nombre);
+            System.out.println("Apellidos: " + usuario.apellidos);
+            System.out.println("Email: " + usuario.email);
+            System.out.println("----------------------||||---------------------------");
+        }
     }
 
-    public static void fechaDeHoy() {
-        Date diaHoy = new Date();
-        System.out.println("Fecha y hora actual: " + diaHoy);
+    public static Usuario[] arrayUsur(int tamanyo) {
+    	String nombre;
+    	String apellidos;
+    	String email;
+        Scanner sc = new Scanner(System.in);
+        Usuario[] usuarios = new Usuario[tamanyo];
+
+        for (int i = 0; i < tamanyo; i++) {
+            System.out.println("Escribe la informacion del Usuario " + (i + 1) + ":");
+            System.out.print("Nombre: ");
+             nombre = sc.nextLine();
+
+            System.out.print("Apellidos: ");
+             apellidos = sc.nextLine();
+
+            System.out.print("Email: ");
+             email = sc.nextLine();
+
+            
+            usuarios[i] = new Usuario(nombre, apellidos, email);
+        }
+
+        return usuarios;
     }
 }
