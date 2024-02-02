4f18a2097333fb22cf7fdf39a7f22d17276b7584
JaviTank <158140993+JaviTank13@users.noreply.github.com>
2024-02-02 11:04:22 +0100

    Actualizado Principal.java
    Actualizado con el nuevo método especificado en los apuntes

--- Principal.java
+++ Principal.java
@@ -1,7 +1,13 @@
-public class Principal {
-  public static void main (String [] args) {
-  
-  System.out.println("Hola Mundo");
-  }
+import java.util.Date;
 
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        fechaDeHoy();
+    }
+
+    public static void fechaDeHoy() {
+        Date diaHoy = new Date();
+        System.out.println("Fecha y hora actual: " + diaHoy);
+    }
 }
